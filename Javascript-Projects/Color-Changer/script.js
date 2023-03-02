cdfb8db870e2542845ede6a89b6a25137c4d34d6
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-03-02 23:39:24 +0530

    js

---
+++ Javascript-Projects/Color-Changer/script.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("btn");
+const randomColor = () =>{
+    let val = "1234567890ABCDEF";
+    let cons = "#";
+    for ( let i = 0; i < 6; i++){
+        cons = cons + val[Math.floor (Math.random * 16)];
+    }
+    return cons;
+};
