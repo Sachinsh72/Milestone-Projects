9486c55dc0c3a22a28c86aa430e2f463ff2f5737
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-07-22 14:56:12 +0530

    idea app

--- Backend-projects/ideaApp/controller/controllers.js
+++ Backend-projects/ideaApp/controller/controllers.js
@@ -21,5 +21,34 @@ exports.createIdea =(req,res)=>{
 }
 
 // update idea 
+exports.updateIdea =(req,res)=>{
+    //need to read the id passed in the path
 
+    // http://localhost:8080/ideaApp/v1/ideas/1
+    const ideaId = req.params.id;
+
+    //if the idea is present - modify else retrun
+    if(ideas[ideaId]){
+        ideas[ideaId] = req.body;
+        res.status(200).send(ideas[ideaId]);
+    }else{
+        res.status(400).send({
+            message : "idea id passed was not correct"
+        })
+    }
+}
 // delete idea 
+
+exports.deleteIdea = (req,res)=>{
+    //check if present - yes delete, no return err message
+    if(ideas[req.params.id]){
+        delete ideas[req.params.id];
+        res.status(200).send({
+            message :"successfully deleted"
+        })
+    }else{
+        res.status(400).send({
+            message: "wrong idea id"
+        })
+    }
+}
--- Backend-projects/ideaApp/routes/router.js
+++ Backend-projects/ideaApp/routes/router.js
@@ -6,4 +6,10 @@ module.exports = (app) => {
 
     //post call
     app.post("/ideaApp/v1/ideas",ideaController.createIdea);
+
+    //put call
+    app.put("/ideaApp/v1/ideas/:id",ideaController.updateIdea);
+
+    //delete call
+    app.delete("/ideaApp/v1/ideas/:id",ideaController.deleteIdea);
 }
