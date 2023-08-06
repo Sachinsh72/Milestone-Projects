736bee3bdc97c08b8bf131ab49ef8d5d36424583
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-06 18:02:55 +0530

    move image

---
+++ Javascript-Projects/Move-Image/script.js
@@ -0,0 +1,10 @@
+const cartoon = document.querySelector("#cartoon");
+const x = 0;
+const y = 0;
+const speed = 10;
+const windowWidth = window.innerWidth;
+// console.log(windowWidth);
+const windowHeight = window.innerHeight;
+// console.log(windowHeight);
+
+// 
