8de75b57a155f822a6757dc8e91f46fec3e334a7
Sachin Sharma <sachinsharma.7thfeb@gmail.com>
2023-08-07 13:22:41 +0530

    move image

--- Javascript-Projects/Move-Image/script.js
+++ Javascript-Projects/Move-Image/script.js
@@ -1,10 +1,35 @@
-const cartoon = document.querySelector("#cartoon");
-const x = 0;
-const y = 0;
+const moveCarton = document.getElementById("cartoon");
+let x = 0;
+let y = 0;
 const speed = 10;
-const windowWidth = window.innerWidth;
+let windowWidth = window.innerWidth;
 // console.log(windowWidth);
 const windowHeight = window.innerHeight;
 // console.log(windowHeight);
 
-// 
+document.addEventListener("keydown",(e) =>{
+    if(e.key == "ArrowUp"){
+        y -= speed;
+        if(y <= -windowHeight){
+            y = windowHeight;
+        }
+    }else if(e.key == "ArrowDown"){
+        y += speed;
+        if(y >= windowHeight){
+            y = -windowHeight;
+        }
+    }else if(e.key == "ArrowRight"){
+        x += speed;
+        if(x >= windowWidth){
+            x = -windowWidth;
+        }
+    }else if(e.key == "ArrowLeft"){
+        x -= speed;
+        if(x <= -windowWidth){
+            y = windowWidth;
+        }
+    }
+
+    cartoon.style.transform = `translate(${x}px, ${y}px)`;
+
+})
